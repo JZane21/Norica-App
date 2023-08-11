933d6b9d41de480962e07d7d56dc35cd3f190178
Jose Carrasco <jsce2021@gmail.com>
2023-08-11 10:51:59 -0400

    fixing bugs in feat-US001

--- src/auth/components/DataInput.tsx
+++ src/auth/components/DataInput.tsx
@@ -1,12 +1,18 @@
-// interface Props{
-//   textQuestion
-//   placeHolder
-//   questionValue
-//   typeInput
-// }
+interface Props {
+  order: string;
+  typeInput: string;
+}
 
-// export const DataInput = () => {
-//   return (
-//     <div>DataInput</div>
-//   )
-// }
+export const DataInput = ({ order, typeInput }: Props) => {
+  return (
+    <>
+      <label
+        htmlFor={typeInput}
+        className="block mb-2 text-sm font-medium text-gray-900
+      dark:text-white w-full"
+      >
+        {order}
+      </label>
+    </>
+  );
+};
--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -1,38 +1,265 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
 import { FieldValues, useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../firebase/firebase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { RegisterError } from "../../models/registerModels";
+import { ModalPage } from "../../modals/ModalPage";
+import { ModalMessage } from "../../modals/ModalMessage";
+import { formValuesRegister } from "../../models/registerModels";
+import { RegisterQuestion } from "../components/RegisterQuestion";
+import { ModalConfirmation } from "../../modals/ModalConfirmation";
 
 export const RegisterPage = () => {
-  const { register, handleSubmit } = useForm();
-  const [error, setError] = useState<boolean>(false);
+  const { register, handleSubmit, watch } = useForm<formValuesRegister>();
 
   const navigate = useNavigate();
 
+  const [existError, setExistError] = useState<boolean>(false);
+  const [created, setCreated] = useState<boolean>(false);
+  const [confirmSend, setConfirmSend] = useState<boolean>(false);
+  const [dataForm, setDataForm] = useState<FieldValues>([]);
+
+  const emailVerification = (value: boolean) => {
+    setCreated(value);
+  };
+
+  const email = watch("email");
+  const password = watch("password");
+  const confirm_password = watch("confirm_password");
+
+  const confirmSubmit = (data: FieldValues) => {
+    // whenSubmit(data);
+    setDataForm(data);
+    setConfirmSend(true);
+  };
+
+  const createNewUser = () => {
+    setConfirmSend(false);
+    whenSubmit(dataForm);
+  };
+
   const whenSubmit = async (data: FieldValues) => {
     const { email, password, confirm_password } = data;
-    if (password === confirm_password) {
+    if (![email, password, confirm_password].includes("")) {
       await createUserWithEmailAndPassword(auth, email, password)
         .then(() => {
-          alert("Usuario Creado");
+          if (
+            !(
+              registerError.email.state ||
+              registerError.password.state ||
+              registerError.confirm_password.state
+            ) &&
+            ![email, password, confirm_password].includes("")
+          ) {
+            const auxUser = auth.currentUser;
+            if (auxUser !== null) {
+              sendEmailVerification(auxUser)
+                .then(() => {
+                  setCreated(true);
+                })
+                .catch((err) => {
+                  alert("error encontrado");
+                });
+            }
+          } else {
+            setExistError(true);
+          }
         })
         .catch((err) => {
-          console.error(err);
-          setError(true);
-        })
-        .finally(() => {
-          if (!error) {
-            navigate("/auth/login");
-          } else {
-            console.error("Internal Error");
-          }
+          setExistError(true);
         });
+    } else {
+      setExistError(true);
     }
   };
 
+  const [clickOne, setClickOne] = useState<boolean>(false);
+  const [clickTwo, setClickTwo] = useState<boolean>(false);
+  const [clickThree, setClickThree] = useState<boolean>(false);
+
+  const [registerError, setRegisterError] = useState<RegisterError>({
+    email: {
+      title: "",
+      message: "",
+      state: false,
+    },
+    password: {
+      title: "",
+      message: "",
+      state: false,
+    },
+    confirm_password: {
+      title: "",
+      message: "",
+      state: false,
+    },
+  });
+
+  const setOnClick = (id: string, value: boolean) => {
+    if (id === "email") {
+      setClickOne(value);
+    } else if (id === "password") {
+      setClickTwo(value);
+    } else {
+      setClickThree(value);
+    }
+  };
+
+  useEffect(() => {
+    if (clickOne) {
+      const emailTester = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      if (!emailTester.test(email)) {
+        if (!registerError.email.state) {
+          setRegisterError({
+            ...registerError,
+            email: {
+              ...registerError.email,
+              message: "* Debe ingresar un email valido",
+              state: true,
+            },
+          });
+        }
+      } else {
+        setRegisterError({
+          ...registerError,
+          email: {
+            ...registerError.email,
+            message: "",
+            state: false,
+          },
+        });
+      }
+    }
+  }, [email]);
+
+  useEffect(() => {
+    if (clickTwo) {
+      const passwordVerificator =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{8,}$/;
+      const lengthPassword = /^.{8,}$/;
+      const oneSmallLetter = /[a-z]/;
+      const oneBigLetter = /[A-Z]/;
+      const oneNumber = /\d/;
+      if (!passwordVerificator.test(password)) {
+        let auxTextError = "";
+        if (!lengthPassword.test(password)) {
+          auxTextError = "* La contraseña debe contener al menos 8 caracteres";
+        } else if (!oneSmallLetter.test(password)) {
+          auxTextError = "* La contraseña debe tener al menos 1 minúscula";
+        } else if (!oneBigLetter.test(password)) {
+          auxTextError = "* La contraseña debe tener al menos 1 mayúscula";
+        } else if (!oneNumber.test(password)) {
+          auxTextError = "* La constraseña debe tener al menos 1 número";
+        }
+        setRegisterError({
+          ...registerError,
+          password: {
+            ...registerError.password,
+            message: auxTextError,
+            state: true,
+          },
+        });
+      } else {
+        setRegisterError({
+          ...registerError,
+          password: {
+            ...registerError.password,
+            message: "",
+            state: false,
+          },
+        });
+      }
+    }
+  }, [password]);
+
+  useEffect(() => {
+    if (clickThree) {
+      if (confirm_password !== password) {
+        if (!registerError.confirm_password.state) {
+          setRegisterError({
+            ...registerError,
+            confirm_password: {
+              ...registerError.confirm_password,
+              message: "* Las contraseñas deben ser iguales",
+              state: true,
+            },
+          });
+        }
+      } else {
+        setRegisterError({
+          ...registerError,
+          confirm_password: {
+            ...registerError.confirm_password,
+            message: "",
+            state: false,
+          },
+        });
+      }
+    }
+  }, [confirm_password]);
+
+  const REGISTER_DATA = [
+    {
+      order: "Ingrese su email",
+      typeInput: "email",
+      placeHolder: "name@company.com",
+      htmlFor: "email",
+      id: "email",
+    },
+    {
+      order: "Cree una contraseña",
+      typeInput: "password",
+      placeHolder: "contraseña",
+      htmlFor: "password",
+      id: "password",
+    },
+    {
+      order: "Confirmar contraseña",
+      typeInput: "password",
+      placeHolder: "contraseña",
+      htmlFor: "confirm-password",
+      id: "confirm_password",
+    },
+  ];
+
   return (
     <>
+      {(confirmSend || created || existError) && (
+        <ModalPage>
+          <>
+            {existError && (
+              <ModalMessage
+                action={setExistError}
+                title="Error de Registro"
+                message={`Los campos no fueron llenados correctamente\m
+                O el usuario ya existe`}
+              />
+            )}
+            {created && (
+              <ModalMessage
+                action={emailVerification}
+                title="Verificación Exitosa"
+                message={`Se envió un correo de confirmación a: ${email}`}
+              />
+            )}
+            {confirmSend && (
+              <ModalConfirmation
+                actionOne={createNewUser}
+                actionTwo={() => setConfirmSend(false)}
+                title="Confirmar Registro"
+                message={`Se registrará el siguiente email:\n${email}\n
+                Se le notificará su registro por correo y por este medio.${" "}
+                En caso de no recibir nada,${" "}revise sus datos e
+                inténtelo nuevamente`}
+              />
+            )}
+          </>
+        </ModalPage>
+      )}
       <section className="bg-gray-50 dark:bg-gray-900">
         <div
           className="flex flex-col items-center justify-center px-6 py-8 mx-auto
@@ -53,85 +280,34 @@ export const RegisterPage = () => {
               <form
                 className="space-y-4 md:space-y-6"
                 action="#"
-                onClick={handleSubmit((data) => whenSubmit(data))}
+                onSubmit={handleSubmit((data) => confirmSubmit(data))}
               >
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium
-                  text-gray-900 dark:text-white"
-                  >
-                    Ingrese su email
-                  </label>
-                  <input
-                    type="email"
-                    {...register("email", { required: true })}
-                    id="email"
-                    className="bg-gray-50 border border-gray-300
-                  text-gray-900 sm:text-sm rounded-lg
-                  focus:ring-primary-600 focus:border-primary-600
-                  block w-full p-2.5 dark:bg-gray-700
-                  dark:border-gray-600 dark:placeholder-gray-400
-                  dark:text-white dark:focus:ring-blue-500
-                  dark:focus:border-blue-500 outline-none"
-                    placeholder="name@company.com"
-                    required={true}
+                {REGISTER_DATA.map((item) => (
+                  <RegisterQuestion
+                    key={item.id}
+                    id={item.id}
+                    order={item.order}
+                    htmlFor={item.htmlFor}
+                    placeHolder={item.placeHolder}
+                    typeInput={item.typeInput}
+                    emailState={registerError.email.state}
+                    emailMessage={registerError.email.message}
+                    passwordState={registerError.password.state}
+                    passwordMessage={registerError.password.message}
+                    confirmPasswordState={registerError.confirm_password.state}
+                    confirmPasswordMessage={
+                      registerError.confirm_password.message
+                    }
+                    register={register}
+                    setOnClick={setOnClick}
                   />
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium
-                  text-gray-900 dark:text-white"
-                  >
-                    Cree una contraseña
-                  </label>
-                  <input
-                    type="password"
-                    {...register("password", { required: true })}
-                    id="password"
-                    placeholder="contraseña"
-                    className="bg-gray-50 border border-gray-300
-                  text-gray-900 sm:text-sm rounded-lg
-                  focus:ring-primary-600
-                  focus:border-primary-600 block w-full p-2.5
-                  dark:bg-gray-700 dark:border-gray-600
-                  dark:placeholder-gray-400 dark:text-white
-                  dark:focus:ring-blue-500
-                  dark:focus:border-blue-500 outline-none"
-                    required={true}
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="confirm-password"
-                    className="block mb-2 text-sm font-medium
-                  text-gray-900 dark:text-white"
-                  >
-                    Confirmar contraseña
-                  </label>
-                  <input
-                    type="password"
-                    {...register("confirm_password", { required: true })}
-                    id="confirm-password"
-                    placeholder="contraseña"
-                    className="bg-gray-50 border border-gray-300
-                  text-gray-900 sm:text-sm rounded-lg
-                  focus:ring-primary-600
-                  focus:border-primary-600 block w-full p-2.5
-                  dark:bg-gray-700 dark:border-gray-600
-                  dark:placeholder-gray-400 dark:text-white
-                  dark:focus:ring-blue-500
-                  dark:focus:border-blue-500 outline-none"
-                    required={true}
-                  />
-                </div>
+                ))}
                 <button
                   type="submit"
                   className="w-full text-white bg-red-600
                 hover:bg-red-500 active:bg-red-700
                 font-medium rounded-lg text-sm px-5 py-2.5
-                text-center"
+                text-center shadow-md"
                 >
                   Crear cuenta
                 </button>
@@ -141,7 +317,7 @@ export const RegisterPage = () => {
                   className="w-full text-white bg-red-600
                 hover:bg-red-500 active:bg-red-700
                 font-medium rounded-lg text-sm px-5 py-2.5
-                text-center"
+                text-center shadow-md"
                 >
                   Volver
                 </button>
