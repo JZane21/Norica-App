af5dc2d2f599cf8fa294402089a26494c9582941
lizarazu22 <lizarazuaramayoignacio@gmail.com>
2023-08-24 18:21:41 -0400

    navegacion entre trabajos , fondo trasparente y resaltado de imagenes con clik

--- src/app/components/FinishedProductCard.tsx
+++ src/app/components/FinishedProductCard.tsx
@@ -1,29 +1,87 @@
-import { Image } from "./Image"
+import React, { useState } from "react";
+import Slider from "react-slick";
+import { Image } from "./Image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 interface Props {
-  productName: string,
-  productImage: string,
-  productDescription: string
+  productName: string;
+  productImage: string;
+  productDescription: string;
 }
 
 export const FinishedProductCard = ({
   productName,
   productImage,
   productDescription,
-  }: Props) => {
-    return(
-      
-<div className="w-[300px] h-[500px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <Image urlImg={productImage} widthLogo={"big"}/>
-    <div className="p-5">
-        <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{productName}</h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{productDescription}</p>
-             <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns={"http://www.w3.org/2000/svg"} fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-    </div>
-</div>
+}: Props) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
 
-    )
+  const [isZoomed, setIsZoomed] = useState(false);
+
+  const handleZoomClick = () => {
+    setIsZoomed(!isZoomed);
+  };
+
+  const handleCloseClick = () => {
+    setIsZoomed(false);
+  };
+
+  return (
+    <div
+      className={`w-[300px] h-[500px] bg-black bg-opacity-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-900 ${
+        isZoomed ? "z-10 fixed inset-0 flex justify-center items-center bg-opacity-90 backdrop-blur" : ""
+      }`}
+    >
+      <Slider {...settings}>
+        <div onClick={handleZoomClick}>
+          <Image urlImg={productImage} widthLogo={"big"} />
+        </div>
+        {/* Agrega más elementos aquí para cada tarjeta de trabajo */}
+      </Slider>
+      {isZoomed && (
+        <div className="absolute top-0 right-0 m-2">
+          <button
+            className="text-white text-xl bg-gray-800 p-1 rounded"
+            onClick={handleCloseClick}
+          >
+            X
+          </button>
+        </div>
+      )}
+      <div className={`p-5 ${isZoomed ? "hidden" : ""}`}>
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-gray-900">
+            {productName}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-400 dark:text-gray-700">
+          {productDescription}
+        </p>
+        <svg
+          className="w-3.5 h-3.5 ml-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </div>
+    </div>
+  );
 };
--- src/app/pages/TrabajosPage.tsx
+++ src/app/pages/TrabajosPage.tsx
@@ -1,11 +1,10 @@
+import React from "react";
 import { FinishedProductCard } from "../components/FinishedProductCard";
 import picTrabajo1 from "../../assets/picTrabajo1.jpg";
 import picTrabajo2 from "../../assets/picTrabajo2.jpg";
 import picTrabajo3 from "../../assets/picTrabajo3.jpg";
 
-
 export const TrabajosPage = () => {
-
   const projectData = [
     {
       productName: 'Proyecto 1',
@@ -23,25 +22,42 @@ export const TrabajosPage = () => {
       productImage: picTrabajo3
     },
     {
-      productName: 'Proyecto 3',
-      productDescription: 'Descripción del proyecto 3',
+      productName: 'Proyecto 4',
+      productDescription: 'Descripción del proyecto 4',
       productImage: picTrabajo3
     },
+    {
+      productName: 'Proyecto 5',
+      productDescription: 'Descripción del proyecto 5',
+      productImage: picTrabajo3
+    },
+    {
+      productName: 'Proyecto 6',
+      productDescription: 'Descripción del proyecto 6',
+      productImage: picTrabajo3
+    },
+    {
+      productName: 'Proyecto 7',
+      productDescription: 'Descripción del proyecto 7',
+      productImage: picTrabajo3
+    },
+    // Agrega más proyectos aquí
   ];
 
+  
   return (
-    <div className = "h-full w-full">
+    <div className="h-full w-full">
       <h1>Proyectos terminados</h1>
-      <ul className = "flex flex-row">
-        {projectData.map((item) => (
+      <div className="flex flex-row">
+        {projectData.map((item, index) => (
           <FinishedProductCard
-          key={item.productName}
-          productName={item.productName}
-          productDescription={item.productDescription}
-          productImage={item.productImage}
+            key={index}
+            productName={item.productName}
+            productDescription={item.productDescription}
+            productImage={item.productImage}
           />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
